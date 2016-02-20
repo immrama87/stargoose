eb4dedd7cb0e3dd41949e2986435c551f29c96c7
James Miner <immrama87@gmail.com>
2016-02-20 17:58:35 -0500

    Started world scroll/player orientation. Added spot lights for corridor

---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldController : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.05f);
+	}
+}
